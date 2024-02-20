b1998617b78ce652ace8b1e263d9e28c7c64154e
Carlo Pisacane <cfpisaca@syr.edu>
2024-02-20 00:06:18 -0500

    Added frontend framework

--- backend/src/main/java/com/example/collectionTracker/CollectionTracker.java
+++ backend/src/main/java/com/example/collectionTracker/CollectionTracker.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CollectionTrackerApplication {
+public class CollectionTracker {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CollectionTrackerApplication.class, args);
+		SpringApplication.run(CollectionTracker.class, args);
 	}
 
 }
