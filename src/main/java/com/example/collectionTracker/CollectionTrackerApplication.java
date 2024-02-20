08ac437208257fa0c7ea11a699c987262addf6d6
Carlo Pisacane <cfpisaca@syr.edu>
2024-02-19 21:42:37 -0500

    Uploaded Spring framework

---
+++ src/main/java/com/example/collectionTracker/CollectionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectionTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collectionTracker/CollectionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collectionTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
