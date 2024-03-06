3e0dce8e5deaacb3da2a9df7377f16ee42c020b6
Carlo Pisacane <cfpisaca@syr.edu>
2024-03-06 03:13:51 -0500

    Register.css no longer affects userpage

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -57,73 +57,75 @@ function Register() {
     };
 
     return (
-        <div className="register-form">
-            <h1>CollectionTracker</h1>
-            <form onSubmit={handleSubmit}>
-            <h2>Create a new account</h2>
-            <hr className="divider" />
-                <div className="name-fields">
-                    <input
-                        type="text"
-                        name="firstname"
-                        placeholder="First Name"
-                        value={formData.firstname}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="lastname"
-                        placeholder="Last Name"
-                        value={formData.lastname}
-                        onChange={handleChange}
-                    />
-                </div>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={formData.username}
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    name="emailOrMobile"
-                    placeholder="Mobile number or email"
-                    value={formData.emailOrMobile}
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="New password"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-                <label htmlFor="birthdate">Date of Birth</label>
-                <input
-                    type="date"
-                    name="birthdate"
-                    value={formData.birthdate}
-                    onChange={handleChange}
-                />
-                <label htmlFor="gender">Gender</label>
-                <select name="gender" value={formData.gender} onChange={handleChange}>
-                    <option value="">Select Gender</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="other">Other</option>
-                </select>
-                <button type="submit">Sign Up</button>
-            </form>
-            {/* Error message */}
-            {formErrors.length > 0 && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <p>{formErrors[0]}</p>
-                        <button onClick={handleDismissError}>OK</button>
+        <div className="page-wrapper">
+            <div className="register-form">
+                <h1>CollectionTracker</h1>
+                <form onSubmit={handleSubmit}>
+                <h2>Create a new account</h2>
+                <hr className="divider" />
+                    <div className="name-fields">
+                        <input
+                            type="text"
+                            name="firstname"
+                            placeholder="First Name"
+                            value={formData.firstname}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="lastname"
+                            placeholder="Last Name"
+                            value={formData.lastname}
+                            onChange={handleChange}
+                        />
                     </div>
-                </div>
-            )}
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="emailOrMobile"
+                        placeholder="Mobile number or email"
+                        value={formData.emailOrMobile}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="New password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="birthdate">Date of Birth</label>
+                    <input
+                        type="date"
+                        name="birthdate"
+                        value={formData.birthdate}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="gender">Gender</label>
+                    <select name="gender" value={formData.gender} onChange={handleChange}>
+                        <option value="">Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <button type="submit">Sign Up</button>
+                </form>
+                {/* Error message */}
+                {formErrors.length > 0 && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <p>{formErrors[0]}</p>
+                            <button onClick={handleDismissError}>OK</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
