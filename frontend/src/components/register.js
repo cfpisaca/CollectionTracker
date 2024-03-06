84c1b0337b41c235a44379840eb2c52019e560fa
Carlo Pisacane <cfpisaca@syr.edu>
2024-03-06 14:33:29 -0500

    Homepage basic frontend complete -> add more when work is done on backend

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import './register.css'; // Import CSS file
 
 function Register() {
@@ -115,6 +116,10 @@ function Register() {
                         <option value="other">Other</option>
                     </select>
                     <button type="submit">Sign Up</button>
+                    {/* Already have an account link */}
+                    <div className="have-account">
+                        <p><Link to="/">Already have an account? </Link></p>
+                    </div>
                 </form>
                 {/* Error message */}
                 {formErrors.length > 0 && (
