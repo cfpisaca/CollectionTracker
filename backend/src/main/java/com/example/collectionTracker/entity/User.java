4edc7b07160aa48d2433d6b9524bfd1c05174df4
Joseph Onwe <jonwe23@icloud.com>
2024-03-26 13:12:05 -0400

    Backend set up for account information (Joseph)

---
+++ backend/src/main/java/com/example/collectionTracker/entity/User.java
@@ -0,0 +1,88 @@
+package com.example.collectionTracker.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users") // Define the table name in the database
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String emailOrMobile;
+    private String password;
+    private String birthdate;
+    private String gender;
+
+    // Getter and Setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailOrMobile() {
+        return emailOrMobile;
+    }
+
+    public void setEmailOrMobile(String emailOrMobile) {
+        this.emailOrMobile = emailOrMobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ backend/src/main/java/com/example/collectionTracker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.collectionTracker.repository;
+
+import com.example.collectionTracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
