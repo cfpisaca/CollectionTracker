a015b622747084c1c6b6361d392d42a826fbf0b9
Carlo Pisacane <cfpisaca@syr.edu>
2024-04-04 02:15:49 -0400

    Edit createListing to new format (not done yet)

--- frontend/src/components/createListing.js
+++ frontend/src/components/createListing.js
@@ -2,19 +2,29 @@ import React from 'react';
 import './createListing.css';
 
 function CreateListing() {
+    const handleBack = () => {
+        window.history.back();
+    };
+
     return (
         <div className="CreateListing">
             <header className="header">
-                <div className="title-section">
-                    <h1>CollectionTracker</h1>
-                </div>
+                <button onClick={handleBack} className="back-button">X</button>
+                <h1>CollectionTracker</h1>
             </header>
             <main className="main-content">
                 <aside className="sidebar">
-                    {/* Keep the sidebar empty */}
+                    <h2>Item for Sale</h2>
+                    <div className="buttons-container">
+                        <button className="button-style">Add Photos</button>
+                        <button className="button-style">Add Video</button>
+                    </div>
+                    <h3>Required</h3>
+                    <input type="text" placeholder="Title" className="input-box" />
+                    <input type="number" placeholder="Price" className="input-box" />
+                    <textarea placeholder="Description" className="input-box"></textarea>
                 </aside>
                 <section id="background">
-                    {/* Content for Create Listing */}
                 </section>
             </main>
         </div>
